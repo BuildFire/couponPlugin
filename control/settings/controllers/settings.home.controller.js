95097683104ec3c764d749ca1a7f6b168bb8cae9
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-05-25 17:03:03 +0530

    Angularized settings section.

---
+++ control/settings/controllers/settings.home.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular, window) {
+  angular
+    .module('couponPluginSettings')
+    .controller('SettingsHomeCtrl', ['$scope',
+      function ($scope) {
+        var SettingsHome = this;
+      }]);
+})(window.angular);
