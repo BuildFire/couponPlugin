9c516c63d485306d10593128a81acf1b10c722c7
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-05-25 16:51:15 +0530

    Angularized content section. Separated all htmls in different files

---
+++ control/content/controllers/content.home.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function (angular, buildfire) {
+  angular
+    .module('couponPluginContent')
+    .controller('ContentHomeCtrl', ['$scope',
+      function ($scope) {
+
+        var ContentHome = this;
+
+      }]);
+})(window.angular, window.buildfire);
---
+++ control/content/controllers/content.item.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+(function (angular, buildfire) {
+  angular
+    .module('couponPluginContent')
+    .controller('ContentItemCtrl', ['$scope',
+      function ($scope) {
+        var ContentItem = this;
+
+      }]);
+})(window.angular, window.buildfire);
