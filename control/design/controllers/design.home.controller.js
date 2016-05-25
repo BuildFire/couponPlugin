7ae2fbdd3599db9b2b12b956e16d9e006e5d563c
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-05-25 16:58:49 +0530

    Angularized design section.

---
+++ control/design/controllers/design.home.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular, window) {
+  angular
+    .module('couponPluginDesign')
+    .controller('DesignHomeCtrl', ['$scope',
+      function ($scope) {
+        var DesignHome = this;
+      }]);
+})(window.angular);
