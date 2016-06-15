3584be7a85baa3705f4e28a7fec041e8c9e70645
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-06-15 17:16:27 +0530

    Implemented remove saved item on saved coupon page and made bottom tabs dynamic

--- widget/controllers/widget.saved.controller.js
+++ widget/controllers/widget.saved.controller.js
@@ -8,6 +8,8 @@
         WidgetSaved.busy = false;
         WidgetSaved.items = [];
         WidgetSaved.savedItems = {};
+        WidgetSaved.hasAtleastOneSavedItem = false;
+
         var searchOptions = {
           skip: 0,
           limit: PAGINATION.itemCount
@@ -71,12 +73,70 @@
           DataStore.search(searchOptions, TAG_NAMES.COUPON_ITEMS).then(successAll, errorAll);
         };
 
+        WidgetSaved.removeSavedItem = function (item, index) {
+          Buildfire.spinner.show();
+          var successRemove = function (result) {
+            Buildfire.spinner.hide();
+            WidgetSaved.items.splice(index, 1);
+            WidgetSaved.getSavedItems();
+            if (!$scope.$$phase)
+              $scope.$digest();
+            var removeSavedItemModal = $modal.open({
+              templateUrl: 'templates/Saved_Removed.html',
+              size: 'sm',
+              backdropClass: "ng-hide"
+            });
+            $timeout(function () {
+              removeSavedItemModal.close();
+            }, 3000);
+
+          }, errorRemove = function () {
+            Buildfire.spinner.hide();
+            return console.error('There was a problem removing your data');
+          };
+          console.log("****************,", item.savedId, WidgetSaved.currentLoggedInUser._id);
+          UserData.delete(item.savedId, TAG_NAMES.COUPON_SAVED, WidgetSaved.currentLoggedInUser._id).then(successRemove, errorRemove)
+        };
+
         WidgetSaved.loadMore = function () {
           console.log("===============In loadmore");
           if (WidgetSaved.busy) return;
           WidgetSaved.busy = true;
           WidgetSaved.getItems();
         };
+
+        WidgetSaved.showListItems = function () {
+          ViewStack.popAllViews()
+        };
+
+        WidgetSaved.showMapView = function () {
+          ViewStack.push({
+            template: 'Map',
+            params: {
+              controller: "WidgetMapCtrl as WidgetMap"
+            }
+          });
+        };
+
+        WidgetSaved.showFilter = function () {
+          ViewStack.push({
+            template: 'Filter',
+            params: {
+              controller: "WidgetFilterCtrl as WidgetFilter"
+            }
+          });
+        };
+
+        /**
+         * Check for current logged in user, if not show ogin screen
+         */
+        buildfire.auth.getCurrentUser(function (err, user) {
+          console.log("===========LoggedInUser", user);
+          if (user) {
+            WidgetSaved.currentLoggedInUser = user;
+            $scope.$apply();
+          }
+        });
 
         WidgetSaved.init();
 
