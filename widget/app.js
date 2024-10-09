0eb1c4c0975cc3793b5f3caf719f0c523ac96beb
alialaqrabawi3 <aalaqrabawi@madaincorp.com>
2024-10-09 18:11:34 +0300

    fix(safe-area): show camera control when safe area applied

--- widget/app.js
+++ widget/app.js
@@ -381,7 +381,19 @@
             });
           });
       };
+      const applySafeAreaStyles = () => {
+        const { navbarEnabled } = buildfire.getContext();
+        const rootElement = document.querySelector('html');
+        const isSafeAreaEnabled = rootElement.getAttribute('safe-area') === 'true';
+        if (isSafeAreaEnabled) {
+          if (!navbarEnabled) {
+            const body = document.querySelector('body');
+            body.classList.add('has-safe-area');
+          }
+        }
+      };
 
       initGoogleMapsSDK();
+      applySafeAreaStyles();
     }])
 })(window.angular, window.buildfire, window);
