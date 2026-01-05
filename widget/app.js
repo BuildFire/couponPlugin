f2e23dc53eed15b0cb0de5b1576986eea3bf7bce
alialaqrabawi3 <aalaqrabawi@madaincorp.com>
2026-01-05 18:33:51 +0300

    apply safe area

--- widget/app.js
+++ widget/app.js
@@ -368,27 +368,14 @@
             });
           });
       };
-      const applySafeAreaStyles = () => {
-        const { navbarEnabled } = buildfire.getContext();
-        const rootElement = document.querySelector('html');
-        const isSafeAreaEnabled = rootElement.getAttribute('safe-area') === 'true';
-        if (isSafeAreaEnabled) {
-          if (!navbarEnabled) {
-            const body = document.querySelector('body');
-            body.classList.add('has-safe-area');
-          }
-        }
-      };
 
       initLanguageStrings()
         .then(() => {
           initGoogleMapsSDK();
-          applySafeAreaStyles();
         })
         .catch((error) => {
           console.error('Error loading language strings', error);
           initGoogleMapsSDK();
-          applySafeAreaStyles();
         });
     }])
 })(window.angular, window.buildfire, window);
